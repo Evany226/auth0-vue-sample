35e1ee4a88751f9e2adc88b8abab383be8f5a328
Evan Yang <71720197+Evany226@users.noreply.github.com>
2022-01-20 12:07:46 -0500

    got auth0 to work on sample

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app" class="d-flex flex-column h-100">
+  <Loading v-if="$auth.isLoading" />
+  <div v-else id="app" class="d-flex flex-column h-100">
     <template>
       <NavBar />
       <div class="container flex-grow-1">
@@ -15,11 +16,13 @@
 <script>
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import Loading from "@/components/Loading";
 
 export default {
   components: {
     Footer,
     NavBar,
+    Loading,
   },
 };
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,17 +3,19 @@
     <nav class="navbar navbar-expand-md navbar-light bg-light">
       <div class="container">
         <div class="navbar-brand logo"></div>
-        <MainNav></MainNav>
+        <MainNav />
+        <AuthNav />
       </div>
     </nav>
   </div>
 </template>
 
 <script>
-import MainNav from "@/components/MainNav";
+  import MainNav from '@/components/MainNav';
+  import AuthNav from '@/components/AuthNav';
 
-export default {
-  name: "NavBar",
-  components: { MainNav },
-};
+  export default {
+    name: 'NavBar',
+    components: { AuthNav, MainNav },
+  };
 </script>
